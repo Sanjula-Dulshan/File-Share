071205dfebc8e5b1f999689318e5247fac181e22
Sanjula-Dulshan <sdulshan10@gmail.com>
2023-12-29 16:00:28 +0530

    style the drop zone

---
+++ web/src/components/DropZone.tsx
@@ -0,0 +1,45 @@
+import React, { useCallback } from "react";
+import { DropzoneOptions, useDropzone } from "react-dropzone";
+
+export const DropZone = () => {
+  const onDrop = useCallback((acceptedFiles: DropzoneOptions[]) => {
+    console.log(acceptedFiles);
+  }, []);
+  const { getRootProps, getInputProps, isDragAccept, isDragReject } =
+    useDropzone({
+      onDrop,
+      multiple: false,
+      accept: {
+        "image/jpeg": [],
+        "image/png": [],
+        "audio/mpeg": [],
+        "application/pdf": [],
+      },
+    });
+  return (
+    <div className="p-4 w-full">
+      <div {...getRootProps()} className="w-full h-80">
+        <input {...getInputProps()} />
+        <div
+          className={
+            "flex flex-col items-center justify-center h-full space-y-3 border border-dashed border-yellow-600 rounded-xl " +
+            (isDragAccept === true ? "border-green-500" : "") +
+            (isDragReject === true ? "border-red-400" : "")
+          }
+        >
+          <img src="/images/folder.png" alt="folder" className="w-16 h-16" />
+          {isDragReject ? (
+            <p>Sorry, This app only supports images, mp3 and pdf</p>
+          ) : (
+            <>
+              <p>Drag & Drop Files Here</p>
+              <p className="mt-2 text-base text-gray-300">
+                Only jpeg, png,pdf & mp3 files supported
+              </p>
+            </>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,10 +1,12 @@
+import { DropZone } from "@/components/DropZone";
+
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center">
       <h1 className="my-4 text-3xl">Got a File? Share it with your friends!</h1>
 
       <div className="flex flex-col items-center justify-center bg-gray-800 shadow-xl w-96 rounded-xl">
-        drop
+        <DropZone />
       </div>
     </div>
   );
