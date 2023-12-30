db23cf6429634243fec400a6fc33d37478b58078
Sanjula-Dulshan <sdulshan10@gmail.com>
2023-12-30 15:45:12 +0530

    share with email backend

--- api/controllers/files.ts
+++ api/controllers/files.ts
@@ -2,6 +2,10 @@ import { UploadApiResponse, v2 as cloudinary } from "cloudinary";
 import File from "../models/File";
 import { IFile } from "../libs/types";
 import https from "https";
+import nodemailer from "nodemailer";
+import { sizeInMb } from "../libs/sizeInMb";
+import emailTemplate from "../utils/emailTemplate";
+import Mail from "nodemailer/lib/mailer";
 
 export const upload = async (req: any, res: any) => {
   try {
@@ -73,4 +77,65 @@ export const downloadFile = async (req: any, res: any) => {
   } catch (error) {
     return res.status(500).json({ msg: "Server Error" });
   }
+};
+
+export const sendEmail = async (req: any, res: any) => {
+  const { id, emailFrom, emailTo, clientOrigin } = req.body;
+
+  console.log("body ", req.body);
+
+  const file = await File.findById(id);
+
+  if (!file) {
+    return res.status(404).json({ msg: "File does not exist" });
+  }
+
+  const transporter = nodemailer.createTransport({
+    //@ts-ignore
+    host: process.env.SENDINBLUE_SMTP_HOST!,
+    port: process.env.SENDINBLUE_SMTP_PORT,
+    auth: {
+      user: process.env.SENDINBLUE_SMTP_USER,
+      pass: process.env.SENDINBLUE_SMTP_PASSWORD,
+    },
+  });
+
+  console.log("transporter ", transporter);
+  console.log(
+    "process.env.SENDINBLUE_SMTP_HOST ",
+    process.env.SENDINBLUE_SMTP_HOST
+  );
+
+  console.log(
+    "process.env.SENDINBLUE_SMTP_PORT ",
+    process.env.SENDINBLUE_SMTP_PORT
+  );
+
+  const { filename, sizeInBytes } = file;
+  const fileSize = sizeInMb(Number(sizeInBytes));
+  const downloadpageLink = `${clientOrigin}/download/${id}`;
+
+  const MailOptions: Mail.Options = {
+    from: emailFrom,
+    to: emailTo,
+    subject: "File shared with you", // Subject line
+    text: `${emailFrom} shared a file with you`,
+    html: emailTemplate(emailFrom, downloadpageLink, filename, fileSize), // html body
+  };
+
+  const info = transporter.sendMail(MailOptions, async (error, info) => {
+    if (error) {
+      console.log("error: ", error);
+      return res.status(500).json({
+        msg: "Server error",
+      });
+    }
+
+    file.sender = emailFrom;
+    file.receiver = emailTo;
+
+    await file.save();
+
+    return res.status(200).json({ msg: "Email sent" });
+  });
 };
